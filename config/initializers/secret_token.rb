d7dd34d1ba843488aa67761d9f10e3092552cb81
Sean Kibler <skibler1223@gmail.com>
2013-03-12 00:25:12 -0400

    Install fresh Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FarmlyWed::Application.config.secret_token = 'c7050fe31aebde6e2a7e8178fcba7a8313d0e5ab2aa11ee1f5ce98c37ead6cc5c2780f4abb0b64414b736b5ea8d7186a91442bf9ae921c980d8581a0bdf3e7a9'
