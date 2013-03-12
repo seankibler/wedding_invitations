a0634a380e911c4068283c189f75992cadd8f1bb
Sean Kibler <skibler1223@gmail.com>
2013-03-12 07:34:56 -0400

    Allow mass assignment of family_id.

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,5 +1,5 @@
 class Guest < ActiveRecord::Base
-  attr_accessible :additional_guests, :city, :first_name, :kids, :last_name, :state, :street, :zip
+  attr_accessible :additional_guests, :city, :first_name, :kids, :last_name, :state, :street, :zip, :family_id
 
   def self.kid_count
     Guest.all.map(&:kids).sum
