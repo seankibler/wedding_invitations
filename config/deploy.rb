594553eadeee32ee0189791d8addb6e69b3add0f
Sean Kibler <skibler1223@gmail.com>
2013-03-12 14:19:25 -0400

    Revert "Sadly revert to ruby 1.8 style hash."
    This reverts commit 0ea78a5f4fce397c388f97507d44ab5ef140cecc.
    
    Conflicts:
    
    	config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,12 +6,10 @@ set :deploy_to, "/var/www/vhosts/#{application}.codingtofreedom.com"
 set :scm, :git
 set :deploy_via, :remote_cache
 
-role :app, "libertas"
 role :web, "libertas"
-role :db, "libertas", :primary => true
+role :app, "libertas"
+role :db,  "libertas", :primary => true
 
-# if you want to clean up old releases on each deploy uncomment this:
-after "deploy:restart", "deploy:cleanup"
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
@@ -21,3 +19,4 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
+after "deploy:restart", "deploy:cleanup"
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-FarmlyWed::Application.config.session_store :cookie_store, :key => '_FarmlyWed_session'
+FarmlyWed::Application.config.session_store :cookie_store, key: '_FarmlyWed_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
