143ed654867d322fba4b2d443e7339ffb4620e69
Sean Kibler <skibler1223@gmail.com>
2013-04-12 07:26:30 -0400

    Regression test creating/updating invitations.

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,11 +1,11 @@
 class Invitation < ActiveRecord::Base
-  attr_accessible :outer_label, :city, :kids, :label, :notes, :state, :street, :wedding_id, :group_id, :zip_code
+  attr_accessible :outer_label, :city, :kids, :size, :notes, :state, :street, :wedding_id, :group_id, :zip_code
 
   belongs_to :wedding
   has_many :guests
   belongs_to :group, class_name: :family, foreign_key: :family_id
 
-  validates :zip, format: {with: /\d{5}(-\d{4})?/, allow_blank: true, message: I18n.translate('validations.invitations.zip')}
+  validates :zip_code, format: {with: /\d{5}(-\d{4})?/, allow_blank: true, message: I18n.translate('validations.invitations.zip_code')}
 
   def address
     "#{self.street} #{self.city}, #{self.state} #{self.zip_code}"
