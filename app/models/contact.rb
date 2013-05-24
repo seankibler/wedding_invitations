fd0b0cbc88f179cc1db85bc9855d2bddba21e12d
Sean Kibler <skibler1223@gmail.com>
2013-05-24 14:13:26 -0400

    Require validation on contact form and show errors if not met.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,7 @@
 class Contact < ActiveRecord::Base
   attr_accessible :name, :email, :message
+  
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :message, presence: true
 end
