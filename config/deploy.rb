04f7d26721fccde28a8ba99c187df88038eefe34
Sean Kibler <skibler1223@gmail.com>
2014-05-08 15:39:31 -0400

    Configures Capistrano recipe for new environment
    - deploys from Github
    - login as root with new server name

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,18 +3,18 @@ require 'whenever/capistrano'
 load 'deploy/assets'
 
 set :application, "farmlywed"
-set :repository,  "ssh://git@bitbucket.org/seankibler/farmlywed.git"
+set :repository,  "git@github.com:seankibler/wedding_invitations.git"
 set :runner, "farmlywed"
 set :group, "farmlywed"
 set :deploy_to, "/var/www/vhosts/#{application}.com"
 set :scm, :git
 set :deploy_via, :remote_cache
 set :whenever_command, 'bundle exec whenever'
+set :user, 'root'
 
 role :web, "do.seankibler.com"
 role :app, "do.seankibler.com"
 role :db,  "do.seankibler.com", :primary => true
-
 
 namespace :deploy do
   task :link_database_config do
@@ -35,5 +35,5 @@ namespace :deploy do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
-after "deploy:update_code", "deploy:link_database_config", "deploy:link_free_accounts", "deploy:fix_upload_permissions"
+before "deploy:assets:precompile", "deploy:link_database_config", "deploy:link_free_accounts", "deploy:fix_upload_permissions"
 after "deploy:restart", "deploy:cleanup"
