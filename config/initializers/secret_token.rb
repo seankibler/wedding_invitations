d24c1457a833663bbd4021d065356dcf3a40fc63
Sean Kibler <skibler1223@gmail.com>
2014-05-07 21:17:05 -0400

    Pulls Rails secret token value from server ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FarmlyWed::Application.config.secret_token = 'c7050fe31aebde6e2a7e8178fcba7a8313d0e5ab2aa11ee1f5ce98c37ead6cc5c2780f4abb0b64414b736b5ea8d7186a91442bf9ae921c980d8581a0bdf3e7a9'
+FarmlyWed::Application.config.secret_token = ENV['SECRET_TOKEN'] 
