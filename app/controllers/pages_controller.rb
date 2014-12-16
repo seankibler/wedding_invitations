8062116eff8536be9202db3d65241e5556c8f0ae
Sean Kibler <skibler1223@gmail.com>
2014-12-16 11:14:13 -0500

    Fixes missing template handling for pages
    - custom 404/500
    - update ruby version in rvmrc

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,18 @@
 class PagesController < ApplicationController
   def show
-    render :action => request.path_info.gsub('-','_').gsub('/','')
+    if requested_page_exists?
+      render :action => path_to_template(request.path_info)
+    else
+      raise ActionController::RoutingError.new "The template #{path_to_template(request.path_info)} does not exist."
+    end
+  end
+
+  private
+  def requested_page_exists?
+    File.exist? "app/views/pages/#{path_to_template(request.path_info)}"
+  end
+
+  def path_to_template(path)
+    path.gsub('-','_').gsub('/','')
   end
 end
